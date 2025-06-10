654158262aa3ed2adabf13b332dde03f8c09c4e9
milad vahdatkhah <89306189+mvahdatkhah@users.noreply.github.com>
2025-06-10 11:19:57 +0330

    Installing Kubernetes components

--- setup_k8s_worker_containerd_stable.sh
+++ setup_k8s_worker_containerd_stable.sh
@@ -78,10 +78,12 @@ sudo tar -C /usr/local/bin -xzf /tmp/nerdctl.tar.gz nerdctl
 rm /tmp/nerdctl.tar.gz
 
 echo "📥 Installing Kubernetes tools kubelet, kubeadm, kubectl v${K8S_VERSION}..."
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v${K8S_VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v${K8S_VERSION}/deb/Release.key | sudo gpg --dearmor --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${K8S_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
 
 echo "📥 Installing kubelet, kubeadm, kubectl..."
 apt-get update
